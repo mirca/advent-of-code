faa7f6ce7671fad2f516ad4b24fb9128bac1aa9b
mirca <jvmirca@gmail.com>
2021-12-16 15:13:03 +0800

    some reshuflling

--- day1/day1.ml
+++ day1/day1.ml
@@ -1,24 +1,6 @@
 (*** PART 1 ***)
 
-(* read input file into a list of strings *)
-(* credit: https://www.rosettacode.org/wiki/Read_a_file_line_by_line *)
-let input_line_opt ic =
-  try Some (input_line ic)
-  with End_of_file -> None
-
-let read_lines ic =
-  let rec aux acc =
-    match input_line_opt ic with
-    | Some line -> aux (line::acc)
-    | None -> (List.rev acc)
-  in
-  aux []
-
-let lines_of_file filename =
-  let ic = open_in filename in
-  let lines = read_lines ic in
-  close_in ic;
-  (lines)
+#use "../lib/helpers.ml"
 
 let lines = lines_of_file "input.txt" ;;
 
---
+++ day2/day2.ml
@@ -0,0 +1,5 @@
+(*** PART 1 ***)
+
+#use "../lib/helpers.ml"
+
+let lines = lines_of_file "input.txt" ;;

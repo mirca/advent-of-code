7109630cbb0b2f886e9e3c97ff81a9f00421129b
mirca <jvmirca@gmail.com>
2021-12-16 21:12:12 +0800

    day 2: part 2, done

--- day2/day2.ml
+++ day2/day2.ml
@@ -30,8 +30,8 @@ let rec get_position l =
 (* returns a list containing depth and horizontal position
  and find out the product of those two quantities *)
 let pos = get_position lines ;;
-let a :: b :: _ = pos ;;
-a * b ;;
+let depth :: horizontal_pos :: _ = pos ;;
+depth * horizontal_pos ;;
 
 (*** PART II ***)
 
@@ -39,7 +39,20 @@ a * b ;;
 let decodes_position_with_aim s aim =
   let command :: value :: _ = String.split_on_char ' ' s in
   match command with
-  | "forward" -> [aim * int_of_string value; int_of_string value; 0]
-  | "down" -> [0; 0; int_of_string value]
-  | "up" -> [0; 0; - int_of_string value]
+  | "forward" -> [0; aim * (int_of_string value); int_of_string value]
+  | "down" -> [int_of_string value; 0; 0]
+  | "up" -> [- int_of_string value; 0; 0]
   | _ -> [0; 0; 0] ;;
+
+(* traverse through the input *)
+let rec get_position_with_aim l aim =
+  match l with
+  | [] -> []
+  | head :: tail -> let aim_increment :: measurements = decodes_position_with_aim head aim in
+                    add_lists (aim_increment :: measurements) (get_position_with_aim tail (aim_increment + aim));;
+
+(* returns a list containing aim, depth and horizontal position
+ and find out the product of those two quantities *)
+let pos = get_position_with_aim lines 0 ;;
+let aim :: depth :: horizontal_pos :: _ = pos ;;
+depth * horizontal_pos ;;

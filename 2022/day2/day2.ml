41f2469d77851119914cd2dc529cf2e3af3941e9
mirca <jvmirca@gmail.com>
2022-12-20 12:12:34 +0800

    day2 part 2 done

--- 2022/day2/day2.ml
+++ 2022/day2/day2.ml
@@ -60,3 +60,51 @@ let rec compute_score lst total =
                                         + game_pay_off head.[0] head.[2])) ;;
 
 compute_score lines 0;;
+
+(* PART II *)
+
+let decode_result result =
+  match result with
+  | 'X' -> "loss"
+  | 'Y' -> "draw"
+  | 'Z' -> "win"
+  | _ -> failwith "not a valid char";;
+
+let inverse_decode_player player =
+  match player with
+  | "rock" -> 'X'
+  | "paper" -> 'Y'
+  | "scissor" -> 'Z'
+  | _ -> failwith "not a valid char";;
+
+let decode_action player1 result =
+  let m_player1 = decode_player player1 in
+  let dec_result = decode_result result in
+  if m_player1 = "rock" then
+    match dec_result with
+    | "loss" -> inverse_decode_player "scissor"
+    | "win" -> inverse_decode_player "paper"
+    | "draw" -> inverse_decode_player "rock"
+    | _ -> failwith "not a valid result"
+  else if m_player1 = "paper" then
+    match dec_result with
+    | "loss" -> inverse_decode_player "rock"
+    | "win" -> inverse_decode_player "scissor"
+    | "draw" -> inverse_decode_player "paper"
+    | _ -> failwith "not a valid result"
+  else
+    match dec_result with
+    | "loss" -> inverse_decode_player "paper"
+    | "win" -> inverse_decode_player "rock"
+    | "draw" -> inverse_decode_player "scissor"
+    | _ -> failwith "not a valid result" ;;
+
+let rec compute_new_score lst total =
+  match lst with
+  | [] -> total
+  | head :: tail -> compute_new_score tail (total + (
+                                        (action_payoff (decode_player(decode_action head.[0] head.[2])))
+                                        + game_pay_off head.[0] (decode_action head.[0] head.[2])
+                                        )) ;;
+
+compute_new_score lines 0;;

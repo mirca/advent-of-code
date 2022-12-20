6a8e30877972102ac10fca0b622846b79c7f0cc9
mirca <jvmirca@gmail.com>
2022-12-20 10:49:29 +0800

    part2 day1 noooob :)

--- 2022/day1/day1.ml
+++ 2022/day1/day1.ml
@@ -4,7 +4,7 @@
 
 let lines = lines_of_file "input.txt" ;;
 
-(* compute max weight *)
+(* compute max calories *)
 let rec get_max lst max_sum =
   match lst with
   | [] -> max_sum
@@ -16,3 +16,16 @@ let rec get_max lst max_sum =
 get_max lines 0;;
 
 (*** PART II ***)
+
+let rec get_top_three lst max1 max2 max3 =
+  match lst with
+  | [] -> max1 + max2 + max3
+  | head :: tail ->
+                  let first_max = max max3 (max max1 max2) in
+                  let second_max = max (min max1 max2) (min (max max1 max2) max3) in
+    match head with
+    | "" -> max (max1 + max2 + max3)
+                (get_top_three tail first_max second_max 0)
+    | s ->  get_top_three tail first_max second_max (max3 + int_of_string s) ;;
+
+get_top_three lines 0 0 0;;
